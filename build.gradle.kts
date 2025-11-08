807b6f599352d2886dd9d1b2a0e0107ee764b4bc
seungju <yunseungju6@gmail.com>
2025-11-09 01:54:37 +0900

    Fix Gradle dependencies and configs, Remove out directory

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,14 @@ plugins {
     id("java")
 }
 
-sourceSets {
-    main {
-        resources {
-            srcDirs("src/main/resources")
-        }
-    }
-}
+//sourceSets {
+//    main {
+//        resources {
+//            srcDirs("src/main/resources")
+//            exclude("game_data/**")
+//        }
+//    }
+//}
 
 group = "com.teampro9"
 version = "1.0-SNAPSHOT"
@@ -23,10 +24,17 @@ repositories {
     mavenCentral()
 }
 
+val junitVersion = "5.10.2"
+
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter:5.12.2")
+    testImplementation(platform("org.junit:junit-bom:$junitVersion"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("org.junit.jupiter:junit-jupiter-params")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
+// 이 블록이 Gradle이 JUnit 5를 사용하도록 지시합니다.
 tasks.test {
     useJUnitPlatform()
 }
