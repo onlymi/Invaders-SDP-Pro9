6e76740fec2c5200285a5edb419f8e9b07bfa270
mangman4 <52250245+mangman4@users.noreply.github.com>
2025-12-07 03:44:58 +0900

    Fix: Fixed an issue where the boss would not hit - Fixed an issue where the boss would not hit

--- src/main/java/entity/Weapon.java
+++ src/main/java/entity/Weapon.java
@@ -98,12 +98,9 @@ public class Weapon extends Entity {
     
     public final void setCharacter(GameCharacter character) {
         this.character = character;
-        
-        this.velocityX = 0;
-        this.velocityY = 0;
-        this.speedX = 0;
-        
         if (this.character != null) {
+            this.velocityX = 0;
+            this.velocityY = 0;
             
             if (this.character.isFacingLeft()) {
                 this.velocityX = -this.speed;
@@ -261,8 +258,6 @@ public class Weapon extends Entity {
      */
     public final void setSpeed(final int speed) {
         this.speed = speed;
-        this.velocityY = speed;
-        this.velocityX = 0;
     }
     
     public void setSpeedX(int speedX) {
